2292f5c78ccd83c692fb29f64a1ee00eaaffce3c
Jonathan Hedstrom <jhedstrom@gmail.com>
2013-08-15 14:19:46 -0700

    Adding a starting puppet manifest.

---
+++ manifests/manifest.pp
@@ -0,0 +1,22 @@
+package { 'php5': }
+package { "php5-cli": }
+package { "php-apc": }
+package { "php5-mysql": }
+package { "php5-gd": }
+package { "php-pear": }
+package { "mysql-server": }
+# Required for drush make
+package { "zip": }
+package { "git": }
+
+service { 'mysql':
+  ensure => 'running',
+  enable => 'true',
+  require => Package['mysql-server'],
+}
+
+exec { "install-drush":
+    unless => "/usr/bin/which drush",
+    command => "/usr/bin/pear channel-discover pear.drush.org; /usr/bin/pear upgrade-all; /usr/bin/pear install drush/drush; /usr/bin/drush -h",
+    require => Package["php-pear"],
+}
